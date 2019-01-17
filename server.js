0fc018d5fa0d079a675fd20dc084611b39f125ea
vic <741472797@qq.com>
2019-01-17 23:15:57 +0800

    更新

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
-var port = process.env.PORT || 8888;
-var qiniu = require('qiniu')
+var port = process.argv[2]
 
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
 
-var server = http.createServer(function (request, response) {
-  var temp = url.parse(request.url, true)
-  var path = temp.pathname
-  var query = temp.query
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var path = request.url 
+  var query = ''
+  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
+  var pathNoQuery = parsedUrl.pathname
+  var queryObject = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
 
-if (path === '/uptoken') {
+  console.log('HTTP 路径为\n' + path)
+  if (path === '/uptoken') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/json', 'utf8')
     response.setHeader('Access-Control-Allow-Origin', '*')//CORS 跨域
-
-    var config = fs.readFileSync('./qiniu-key.json')
-    config = JSON.parse(config)
-
-    let {accessKey,secretKey} = config;
-    var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
-    var options = {
-        scope: '163-music-demo',
-      };
-      var putPolicy = new qiniu.rs.PutPolicy(options);
-      var uploadToken=putPolicy.uploadToken(mac);
-
     response.write(`
-      {
-        "uptoken": "${uploadToken}"
-      }  
-      `)//这不是对象，服务器只能返回字符串
+    {
+      "姓名": "阿经"
+    }
+    `)
     response.end()
-  } else {
+  }else{
     response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.end()
   }
 
@@ -47,6 +41,3 @@ if (path === '/uptoken') {
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
- 
